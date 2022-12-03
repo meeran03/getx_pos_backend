74c3357eec8a358061ce2d8359c6d10eca6b6d05
meeran <muhammadmeeran2003@gmail.com>
2022-12-03 07:18:47 +0500

    Updated

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,6 @@
 // configuration for mssql connector
 const config = {
     connectionString: `Driver=SQL Server;Server=.\\SQLEXPRESS;Database=Getx;Trusted_Connection=False;`,
-    // connectionString: 'Driver=SQL Server;Server=tcp:meeran.database.windows.net,1433;Database=Getx;Uid=meeran;Pwd=DbProject@321;',
     secretOrKey: "secret",
     image_url: "http://localhost:5001/images/",
 };
